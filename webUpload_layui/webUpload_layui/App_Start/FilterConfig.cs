e93b7e5e363762c57181cae7e177e1b94014f400
xuzu <1270091284@qq.com>
2018-09-08 21:30:35 +0800

    新创建

---
+++ webUpload_layui/webUpload_layui/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace webUpload_layui
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ webUpload_layui/webUpload_layui/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="webUpload_layui.MvcApplication" Language="C#" %>
